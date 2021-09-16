e15f88f6d13f52cfcc40b1253cd4ffffedd34334
sepiggy <jms1209@qq.com>
2021-09-17 00:23:38 +0800

    on 6.1

---
+++ demo61/src/main.rs
@@ -0,0 +1,18 @@
+enum IpAddrKind{
+    V4,
+    V6,
+}
+
+#[derive(Debug)]
+enum IpAddr {
+    V4(String),
+    V6(String),
+}
+
+fn main() {
+    let home = IpAddr::V4(String::from("127.0.0.1"));
+    let loopback = IpAddr::V6(String::from("::1"));
+
+    println!("{:#?}", home);
+    println!("{:#?}", loopback);
+}
