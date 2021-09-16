2452351004e9cb543593022f0f85f7e36d9c5bcd
sepiggy <jms1209@qq.com>
2021-09-17 00:09:59 +0800

    ch05 ok

---
+++ demo51/src/main.rs
@@ -0,0 +1,45 @@
+#[derive(Debug)]
+struct User {
+    username: String,
+    email: String,
+    sign_in_count: u64,
+    active: bool,
+}
+
+fn main() {
+    let user1 = User {
+        email: String::from("someone@example.com"),
+        username: String::from("someusername123"),
+        active: true,
+        sign_in_count: 1,
+    };
+    println!("{:#?}", user1);
+
+    let mut user1 = User{
+        email: String::from("someone@example.com"),
+        username: String::from("someusername123"),
+        active: true,
+        sign_in_count: 1,
+    };
+    user1.email = String::from("anotheremail@example.com");
+    println!("{:#?}", user1);
+
+    let user1 = build_user(String::from("jms@qq.com"), String::from("jms"));
+    println!("{:#?}", user1);
+
+    let user2 = User {
+        email: String::from("another@example.com"),
+        username: String::from("anotherusername567"),
+        ..user1
+    };
+    println!("{:#?}", user2);
+}
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        email: email,
+        username: username,
+        active: true,
+        sign_in_count: 1,
+    }
+}
