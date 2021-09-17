1b13462ab8403b56a8bdcd08f7a92a8b9240cdf0
sepiggy <jms1209@qq.com>
2021-09-17 17:02:44 +0800

    on 6.1

--- demo61/src/main.rs
+++ demo61/src/main.rs
@@ -1,18 +1,54 @@
-enum IpAddrKind{
-    V4,
-    V6,
-}
+// enum IpAddrKind{
+//     V4,
+//     V6,
+// }
+//
+// #[derive(Debug)]
+// enum IpAddr {
+//     V4(String),
+//     V6(String),
+// }
+//
+// fn main() {
+//     let home = IpAddr::V4(String::from("127.0.0.1"));
+//     let loopback = IpAddr::V6(String::from("::1"));
+//
+//     println!("{:#?}", home);
+//     println!("{:#?}", loopback);
+// }
 
-#[derive(Debug)]
-enum IpAddr {
-    V4(String),
-    V6(String),
-}
+// #[derive(Debug)]
+// enum IpAddr {
+//     v4(u8, u8, u8, u8),
+//     v6(String),
+// }
+//
+
+// #[derive(Debug)]
+// enum Message {
+//     Quit,
+//     Move { x: i32, y: i32 },
+//     Write(String),
+//     ChangeColor(i32, i32, i32),
+// }
+// 
+// impl Message {
+//     fn call(&self) {
+//         println!("{:#?}", self);
+//     }
+// }
+// 
+// fn main() {
+//     let m = Message::Write(String::from("hello"));
+//     m.call();
+// }
 
 fn main() {
-    let home = IpAddr::V4(String::from("127.0.0.1"));
-    let loopback = IpAddr::V6(String::from("::1"));
+	let some_number = Some(5);
+	let some_string = Some("a_string");
+	let absent_number: Option<i32> = None;
 
-    println!("{:#?}", home);
-    println!("{:#?}", loopback);
+	println!("{:#?}", some_number);
+	println!("{:#?}", some_string);
+	println!("{:#?}", absent_number);
 }
